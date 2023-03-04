0f684a1caa6727f2426a3971e5f48acb4d0b7030
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-03-04 17:36:59 -0500

    added notes in script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,184 +2,28 @@
 // to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, 
 // how could we retrieve geographical coordinates given a city name?
 
+// You will use `localStorage` to store any persistent data. For more information on how 
+// to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API 
+// keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+
+// AS A traveler
+// I WANT to see the weather outlook for multiple cities
+// SO THAT I can plan a trip accordingly
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
 // API link 
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={b4baa4ee379f57320fa20816d25e66c0}
 
 // API Key
 // b4baa4ee379f57320fa20816d25e66c0
 
-// Example code
-{
-    "cod": "200",
-    "message": 0,
-    "cnt": 40,
-    "list": [
-      {
-        "dt": 1661871600,
-        "main": {
-          "temp": 296.76,
-          "feels_like": 296.98,
-          "temp_min": 296.76,
-          "temp_max": 297.87,
-          "pressure": 1015,
-          "sea_level": 1015,
-          "grnd_level": 933,
-          "humidity": 69,
-          "temp_kf": -1.11
-        },
-        "weather": [
-          {
-            "id": 500,
-            "main": "Rain",
-            "description": "light rain",
-            "icon": "10d"
-          }
-        ],
-        "clouds": {
-          "all": 100
-        },
-        "wind": {
-          "speed": 0.62,
-          "deg": 349,
-          "gust": 1.18
-        },
-        "visibility": 10000,
-        "pop": 0.32,
-        "rain": {
-          "3h": 0.26
-        },
-        "sys": {
-          "pod": "d"
-        },
-        "dt_txt": "2022-08-30 15:00:00"
-      },
-      {
-        "dt": 1661882400,
-        "main": {
-          "temp": 295.45,
-          "feels_like": 295.59,
-          "temp_min": 292.84,
-          "temp_max": 295.45,
-          "pressure": 1015,
-          "sea_level": 1015,
-          "grnd_level": 931,
-          "humidity": 71,
-          "temp_kf": 2.61
-        },
-        "weather": [
-          {
-            "id": 500,
-            "main": "Rain",
-            "description": "light rain",
-            "icon": "10n"
-          }
-        ],
-        "clouds": {
-          "all": 96
-        },
-        "wind": {
-          "speed": 1.97,
-          "deg": 157,
-          "gust": 3.39
-        },
-        "visibility": 10000,
-        "pop": 0.33,
-        "rain": {
-          "3h": 0.57
-        },
-        "sys": {
-          "pod": "n"
-        },
-        "dt_txt": "2022-08-30 18:00:00"
-      },
-      {
-        "dt": 1661893200,
-        "main": {
-          "temp": 292.46,
-          "feels_like": 292.54,
-          "temp_min": 290.31,
-          "temp_max": 292.46,
-          "pressure": 1015,
-          "sea_level": 1015,
-          "grnd_level": 931,
-          "humidity": 80,
-          "temp_kf": 2.15
-        },
-        "weather": [
-          {
-            "id": 500,
-            "main": "Rain",
-            "description": "light rain",
-            "icon": "10n"
-          }
-        ],
-        "clouds": {
-          "all": 68
-        },
-        "wind": {
-          "speed": 2.66,
-          "deg": 210,
-          "gust": 3.58
-        },
-        "visibility": 10000,
-        "pop": 0.7,
-        "rain": {
-          "3h": 0.49
-        },
-        "sys": {
-          "pod": "n"
-        },
-        "dt_txt": "2022-08-30 21:00:00"
-      },
-      ....
-      {
-        "dt": 1662292800,
-        "main": {
-          "temp": 294.93,
-          "feels_like": 294.83,
-          "temp_min": 294.93,
-          "temp_max": 294.93,
-          "pressure": 1018,
-          "sea_level": 1018,
-          "grnd_level": 935,
-          "humidity": 64,
-          "temp_kf": 0
-        },
-        "weather": [
-          {
-            "id": 804,
-            "main": "Clouds",
-            "description": "overcast clouds",
-            "icon": "04d"
-          }
-        ],
-        "clouds": {
-          "all": 88
-        },
-        "wind": {
-          "speed": 1.14,
-          "deg": 17,
-          "gust": 1.57
-        },
-        "visibility": 10000,
-        "pop": 0,
-        "sys": {
-          "pod": "d"
-        },
-        "dt_txt": "2022-09-04 12:00:00"
-      }
-    ],
-    "city": {
-      "id": 3163858,
-      "name": "Zocca",
-      "coord": {
-        "lat": 44.34,
-        "lon": 10.99
-      },
-      "country": "IT",
-      "population": 4593,
-      "timezone": 7200,
-      "sunrise": 1661834187,
-      "sunset": 1661882248
-    }
-  }
+
